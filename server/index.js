bfd3a244057d654804023beba78bdcbec9d8a2e6
safwat fathi <safwat.rashwan@gmail.com>
2020-05-02 14:18:35 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello from server</h1>`);
+});
+
+// on socket connect
+io.on("connection", (socket) => {
+  // console.log("a user connected");
+
+  socket.on("drawing", () => {
+    console.log("llllllloooo");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
