e554e9714a5aa84d100d28060be07b7562bd3b61
Safwat_fathi <safwat@iddma.com>
2020-05-16 04:38:30 +0200

    after reverting back before rooms shit

--- client/src/utils/draw.js
+++ client/src/utils/draw.js
@@ -1,22 +1,23 @@
-export default function drawLine(context, x0, y0, x1, y1, socket, emit) {
+export default function drawLine(context, x0, y0, x1, y1, color, client, send) {
   context.beginPath();
   context.moveTo(x0, y0);
   context.lineTo(x1, y1);
   context.lineWidth = 2;
+  context.strokeStyle = color;
   context.stroke();
   context.closePath();
 
-  if (!emit) {
+  if (!send) {
     return;
   }
-  // var w = canvas.width;
-  // var h = canvas.height;
 
-  socket.emit("drawing", {
-    x0: x0,
-    y0: y0,
-    x1: x1,
-    y1: y1,
-    // color: color,
-  });
+  client.send(
+    JSON.stringify({
+      x0,
+      y0,
+      x1,
+      y1,
+      color,
+    })
+  );
 }
---
+++ server/server.js
@@ -0,0 +1,52 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require("websocket").server;
+
+const http = require("http");
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+const wsServer = new webSocketServer({
+  httpServer: server,
+});
+
+// I'm maintaining all active connections in this object
+const clients = {};
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () =>
+    Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  return s4() + s4() + "-" + s4();
+};
+
+wsServer.on("request", function (request) {
+  var userID = getUniqueID();
+
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+
+  console.log("connected: " + userID);
+
+  /* 
+	//////////////
+	custom events
+	//////////////
+	*/
+  connection.on("message", (message) => {
+    let msgString = JSON.stringify(message.utf8Data);
+
+    // Loop through all clients
+    for (var i in clients) {
+      // Send a message to the client with the message
+      clients[i].sendUTF(msgString);
+    }
+  });
+
+  connection.on("close", function (reasonCode, desc) {
+    delete clients[userID];
+    console.log(reasonCode, desc);
+  });
+});
