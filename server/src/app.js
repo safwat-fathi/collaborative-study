c688d1fb90ee84a607dcb24200a4e625c649936c
Safwat_fathi <safwat@iddma.com>
2020-06-15 15:11:01 +0200

    fixing server response to login & register

--- server/src/app.js
+++ server/src/app.js
@@ -11,20 +11,4 @@ const userRoutes = require("./api/routes/user.route");
 
 app.use("/users", userRoutes);
 
-// handle errors if it passed user & room routes
-app.use((req, res, next) => {
-  const error = new Error("Not Found!");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
-});
-
 module.exports = app;
