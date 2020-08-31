608eaae0e4a6cd5f9b7e76d7f85e9b047c3ac095
Safwat_fathi <safwat@iddma.com>
2020-08-31 16:40:56 +0200

    adding controllers (rooms)

--- server/src/api/controllers/room.controller.js
+++ server/src/api/controllers/room.controller.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const Room = require("../models/room.model");
 
 const getRooms = async (req, res, next) => {
@@ -45,4 +48,110 @@ const createRoom = async (req, res, next) => {
   next();
 };
 
-module.exports = { getRooms, createRoom };
+const editRoom = async (req, res, next) => {
+  const { oldName, newName, desc } = req.body;
+
+  let room = await Room.findOneAndUpdate(
+    { name: oldName },
+    { name: newName, desc: desc },
+    { new: true, useFindAndModify: false }
+  );
+
+  if (!room) {
+    return res.status(404).json({
+      message: "no room found to edit",
+    });
+  }
+
+  res.status(200).json({
+    message: "room edited successfully",
+    room,
+  });
+
+  next();
+};
+
+const changeAdmin = async (req, res, next) => {
+  const { oldAdminID, newAdminID } = req.body;
+
+  let room = await Room.findOneAndUpdate(
+    { adminID: oldAdminID },
+    { adminID: newAdminID },
+    { new: true, useFindAndModify: false }
+  );
+
+  if (!room) {
+    return res.status(404).json({
+      message: "no room found to edit",
+    });
+  }
+
+  res.status(200).json({
+    message: "room edited successfully",
+    room,
+  });
+
+  next();
+};
+
+const uploadFiles = async (req, res, next) => {
+  try {
+    let room = await Room.findById(req.params.id);
+
+    if (req.files === null || !room) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    let uploads = [];
+    console.log(req.files);
+    // if (req.files.uploads.length) {
+    //   uploads = req.files.uploads;
+    //   room.uploads = [...uploads, ...room.uploads];
+    // } else {
+    //   uploads = req.files.uploads;
+    //   room.uploads = [uploads, ...room.uploads];
+    // }
+
+    // await room.save();
+
+    // return res.status(200).send(room.uploads);
+    return res.status(200).json({ message: "success" });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: err,
+    });
+  }
+
+  next();
+};
+
+const getFiles = async (req, res, next) => {
+  try {
+    let room = await Room.findById(req.params.id);
+
+    if (!room || !room.uploads) {
+      return res.status(400).json({ message: "No files uploaded" });
+    }
+
+    return res.send(room.uploads);
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: err,
+    });
+  }
+
+  next();
+};
+
+module.exports = {
+  getRooms,
+  createRoom,
+  editRoom,
+  changeAdmin,
+  uploadFiles,
+  getFiles,
+};
--- server/src/api/routes/room.route.js
+++ server/src/api/routes/room.route.js
@@ -1,126 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const fs = require("fs");
-const bcrypt = require("bcrypt");
 
 // contorllers
-const { getRooms, createRoom } = require("../controllers/room.controller");
+const {
+  getRooms,
+  createRoom,
+  editRoom,
+  changeAdmin,
+  uploadFiles,
+  getFiles,
+} = require("../controllers/room.controller");
 
 // middleware
 const auth = require("../middleware/auth");
 
+// -------------
 // get all rooms
+// -------------
 router.get("/", auth, getRooms);
 
+// -------------
 // create room
+// -------------
 router.post("/create", auth, createRoom);
 
+// -------------
 // edit room
-router.post("/edit", async (req, res, next) => {
-  const { oldName, newName, desc } = req.body;
+// -------------
+router.post("/edit", auth, editRoom);
 
-  let room = await Room.findOneAndUpdate(
-    { name: oldName },
-    { name: newName, desc: desc },
-    { new: true, useFindAndModify: false }
-  );
-
-  if (!room) {
-    return res.status(404).json({
-      message: "no room found to edit",
-    });
-  }
-
-  res.status(200).json({
-    message: "room edited successfully",
-    room,
-  });
-
-  next();
-});
-
+// -------------
 // change admin
-router.post("/changeAdmin", async (req, res, next) => {
-  const { oldAdminID, newAdminID } = req.body;
-
-  let room = await Room.findOneAndUpdate(
-    { adminID: oldAdminID },
-    { adminID: newAdminID },
-    { new: true, useFindAndModify: false }
-  );
-
-  if (!room) {
-    return res.status(404).json({
-      message: "no room found to edit",
-    });
-  }
-
-  res.status(200).json({
-    message: "room edited successfully",
-    room,
-  });
-
-  next();
-});
+// -------------
+router.post("/changeAdmin", auth, changeAdmin);
 
 // -------------
 // upload files
 // -------------
-router.post("/:id/uploads", auth, async (req, res, next) => {
-  try {
-    let room = await Room.findById(req.params.id);
-
-    if (req.files === null || !room) {
-      return res.status(400).json({ message: "No file uploaded" });
-    }
-
-    let uploads = [];
-    console.log(req.files);
-    // if (req.files.uploads.length) {
-    //   uploads = req.files.uploads;
-    //   room.uploads = [...uploads, ...room.uploads];
-    // } else {
-    //   uploads = req.files.uploads;
-    //   room.uploads = [uploads, ...room.uploads];
-    // }
-
-    // await room.save();
-
-    // return res.status(200).send(room.uploads);
-    return res.status(200).json({ message: "success" });
-  } catch (err) {
-    console.log(err);
-
-    return res.status(500).json({
-      message: err,
-    });
-  }
-
-  next();
-});
+router.post("/:id/uploads", auth, uploadFiles);
 
 // -------------
 // get uploaded files
 // -------------
-router.get("/:id/uploads", auth, async (req, res, next) => {
-  try {
-    let room = await Room.findById(req.params.id);
-
-    if (!room || !room.uploads) {
-      return res.status(400).json({ message: "No files uploaded" });
-    }
-
-    return res.send(room.uploads);
-  } catch (err) {
-    console.log(err);
-
-    return res.status(500).json({
-      message: err,
-    });
-  }
-
-  next();
-});
+router.get("/:id/uploads", auth, getFiles);
 
 module.exports = router;
